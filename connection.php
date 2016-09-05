59c7b4b6b0d3eefce6c576c5a82038a5a907544b
klemen7 <klemen.klemenko7@gmail.com>
2016-09-05 08:45:06 +0200

    New dump integrated

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $user="root";
 $password="";
 $link = mysqli_connect($host, $user, $password)or 
         die("Povezava neuspešna: ".  mysqli_connect_error());;
-$db_name = "project_klavz";
+$db_name = "project1_klavz";
 mysqli_select_db($link, $db_name) or 
         die("Povezava neuspešna: ".  mysqli_connect_error());
 mysqli_query($link, "SET NAMES 'utf8'");
