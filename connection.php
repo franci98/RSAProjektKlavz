1c6ffa43660ba16592388fd49be533d5ced16763
franci-solski <franciklavz@gmail.com>
2016-09-12 10:05:51 +0200

    fixed connection

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
 $host="localhost";
-$user="root";
-$password="";
+$user="franci";
+$password="kadaj";
 $link = mysqli_connect($host, $user, $password)or 
         die("Povezava neuspešna: ".  mysqli_connect_error());;
-$db_name = "project1_klavz";
+$db_name = "franci";
 mysqli_select_db($link, $db_name) or 
         die("Povezava neuspešna: ".  mysqli_connect_error());
 mysqli_query($link, "SET NAMES 'utf8'");
